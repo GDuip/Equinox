1e2c55ac9e14644a3e30b03685073229eb91189e
GDuip <152042002+GDuip@users.noreply.github.com>
2024-05-19 14:21:43 -0500

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,10 +1,26 @@
-self.__uv$config = {
-    prefix: "/edu/",
-    bare: "/bare/",
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: "/uv/uv.handler.js",
-    bundle: "/uv/uv.bundle.js",
-    config: "/uv/uv.config.js",
-    sw: "/uv/uv.sw.js",
-  };
+self.__dynamic$config = {
+  prefix: "/a/q/",
+  encoding: "xor",
+  mode: "production",
+  logLevel: 0,
+  bare: {
+    version: 2,
+    path: "/ov/",
+  },
+  tab: {
+    title: null,
+    icon: null,
+    ua: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.3",
+  },
+  assets: {
+    prefix: "/dy/",
+    files: {
+      handler: "handler.js?v=12",
+      client: "client.js?v=12",
+      worker: "worker.js?v=12",
+      config: "config.js?v=12",
+      inject: "",
+    },
+  },
+  block: [],
+};
